1b9077c07adbba9961b0c8bf994616b2b4a5a2ae
Martin Lossin <Martin.Lossin@refra.com>
2021-01-21 10:44:34 +0100

    updates

--- api/Controllers/QrCodeController.cs
+++ api/Controllers/QrCodeController.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using QRCoder.Exceptions;
-using QRCoder;
 using System.Drawing;
 using System.IO;
-using System.Net.Http;
+using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
 
-namespace QRCodeGenerator.Controllers
+namespace Refra.QrCodeGenerator.api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/qrcode")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class QrCodeController : ControllerBase
     {
         // GET api/values
         [HttpGet]
@@ -38,24 +37,27 @@ namespace QRCodeGenerator.Controllers
             }
             //ImageConverter converter = new ImageConverter(); //https://stackoverflow.com/questions/7350679/convert-a-bitmap-into-a-byte-array
         }
-        private Byte[] CreateRefraQrCode(string v)
+        private async Task<Byte[]> CreateRefraQrCode(string v)
         {
-            QRCoder.QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(v, QRCoder.QRCodeGenerator.ECCLevel.Q);
-            QRCode qrCode = new QRCode(qrCodeData);
-
-            Bitmap refraIcon = GetRefraLogo().Result; //(Bitmap)Bitmap.FromFile("");            // https://www.refra.com/img/icon/favicon-196x196.png
-            //Bitmap qrCodeImage = qrCode.GetGraphic(20, "#ff4c02", "#000000", true);
-            //Color darColor = System.Drawing.ColorTranslator.FromHtml("#ff4c02");
-            Color darColor = System.Drawing.ColorTranslator.FromHtml("#000000");
-            Color lightColor = System.Drawing.ColorTranslator.FromHtml("#ffffff");
-            Bitmap qrCodeImage = qrCode.GetGraphic(20,darColor ,lightColor , refraIcon,10,5,true);
-            using (var stream = new MemoryStream())
+           return await Task< Byte[]>.Run(() =>
             {
-                qrCodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
-                Byte[] byteBild = stream.ToArray();
-                return byteBild;
-            }
+                QRCoder.QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(v, QRCoder.QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+
+                Bitmap refraIcon = GetRefraLogo().Result; //(Bitmap)Bitmap.FromFile("");            // https://www.refra.com/img/icon/favicon-196x196.png
+                //Bitmap qrCodeImage = qrCode.GetGraphic(20, "#ff4c02", "#000000", true);
+                //Color darColor = System.Drawing.ColorTranslator.FromHtml("#ff4c02");
+                Color darColor = System.Drawing.ColorTranslator.FromHtml("#000000");
+                Color lightColor = System.Drawing.ColorTranslator.FromHtml("#ffffff");
+                Bitmap qrCodeImage = qrCode.GetGraphic(20, darColor, lightColor, refraIcon, 10, 5, true);
+                using (var stream = new MemoryStream())
+                {
+                    qrCodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    Byte[] byteBild = stream.ToArray();
+                    return byteBild;
+                }
+            });
         }
 
         private async Task<Bitmap> GetRefraLogo()
@@ -63,6 +65,7 @@ namespace QRCodeGenerator.Controllers
             var images = new List<Bitmap>();
             using (var client = new HttpClient())
             {
+                /// Sollte gegen statischen Teil getauscht werden... muss man ja nicht immer laden..
                 var response = await client.GetAsync("https://www.refra.com/img/icon/favicon-196x196.png"); // https://www.refra.com/img/icon/favicon-196x196.png
                 //var bitmap = new Bitmap();
                 if (response != null && response.StatusCode == HttpStatusCode.OK)
@@ -81,28 +84,28 @@ namespace QRCodeGenerator.Controllers
 
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(string id)
+        [HttpGet("{message}")]
+        public async Task<ActionResult<string>> Get(string message)
         {
-            return File(CreateRefraQrCode(id), "image/jpeg");
+            return  File(await CreateRefraQrCode(message), "image/jpeg");
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
--- api/Startup.cs
+++ api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -29,7 +30,7 @@ namespace QRCodeGenerator
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -42,6 +43,7 @@ namespace QRCodeGenerator
             }
 
             app.UseHttpsRedirection();
+            //app.UseRouting();
             app.UseMvc();
         }
     }
